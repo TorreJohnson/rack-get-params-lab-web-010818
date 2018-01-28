e3ba50eefdd391a343b475dc76b1aeb89ebc12aa
TorreJohnson <torre.johnson@gmail.com>
2018-01-28 13:45:00 -0500

    completed Rack Routes and GET Params lab

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,8 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
 
+  @@cart = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,11 +15,31 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 0
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      resp.write add_item_to_cart(item_to_add)
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
+  end
+
+  def add_item_to_cart(item)
+    if @@items.include?(item)
+      @@cart << item
+      return "added #{item}"
+    else
+      return "We don't have that item"
+    end
   end
 
   def handle_search(search_term)
